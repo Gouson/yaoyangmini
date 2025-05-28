6a947cb416d12eb35fde63288dd7225a765e94ea
gaokexin <406263733@qq.com>
2025-05-28 17:01:47 +0800

    start

---
+++ pages/login/login.vue
@@ -0,0 +1,28 @@
+<template>
+	<view>
+		登录页面
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+			wx.cloud.callFunction({
+				name: 'abc'
+			}).then(res => {
+				console.log('res', res)
+			})
+		},
+		methods: {
+
+		}
+	}
+</script>
+<style>
+
+</style>
---
+++ unit/cloud.js
@@ -0,0 +1,14 @@
+// utils/cloud.js
+import cloudbase from '@cloudbase/js-sdk';
+
+// 初始化云开发环境
+const cloud = cloudbase.init({
+  env: 'cloud1-8grelvba9540d4e3' // 替换成你自己的环境ID
+});
+
+// 导出数据库和认证实例，方便在其他页面使用
+const db = cloud.database();
+const auth = cloud.auth();
+const _ = db.command; // 用于进行高级查询
+
+export { cloud, db, auth, _ };
